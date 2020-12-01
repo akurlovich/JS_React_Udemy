392a0f9f2f89a08683b90386ced32e47fdc7ccbf
akurlovich <71250705+akurlovich@users.noreply.github.com>
2020-12-01 21:02:43 +0300

    first project

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
-// alert('ghbdtn');
-// const result = confirm('Are you here?');
-// console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
-// const answer = prompt('Are you here?', '');
-// console.log(answer); 
+const personalMovieDB = {
+    count: numberOfFilms, 
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const answers = [];
-answers[0] = prompt('Kak vas zovut?', '');
-answers[1] = prompt('Kak vasha familia?', '');
-answers[2] = prompt('Vash vozrast?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
+console.log(personalMovieDB); 

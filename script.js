e544ad282a84b731f9b97e8780c8e9a411e88861
akurlovich <71250705+akurlovich@users.noreply.github.com>
2020-12-01 23:09:12 +0300

    2

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
+"use strict";
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms, 
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB); 
 
-console.log(personalMovieDB); 
+// if (9 == 9 || 7 == 7) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+// (num >= 50) ? console.log("Ok!") : console.log('Error'); 
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        break;
+    }
+    console.log(i);
+
+}

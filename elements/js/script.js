292e655d836f835778ec53bbbf3fc24045b522c7
akurlovich <71250705+akurlovich@users.noreply.github.com>
2020-12-06 11:57:45 +0300

    find_elements_on_page

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,2 +1,48 @@
 'use strict';
 
+// const box = document.getElementById('box');
+
+// console.log(box);  
+
+// const btns = document.getElementsByTagName('button');
+// //const btns = document.getElementsByTagName('button')[1]; //сразу передаёт элемент 
+
+// console.log(btns[1]);
+
+// const circles = document.getElementsByClassName('circle'); 
+// console.log(circles); // тоже будет коллекция
+
+// const hearts = document.querySelectorAll('.heart'); //тоже будет коллекция, то есть метод forEach
+
+// // console.log(hearts); 
+
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+
+// // const oneHeart = document.querySelector('.heart'); 
+// const oneHeart = document.querySelector('div'); 
+// console.log(oneHeart); 
+
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+
+// box.style.backgroundColor = 'blue'; 
+// box.style.width = '500px';
+
+// box.style.cssText = 'background-color: blue; width: 500px';
+// btns[1].style.borderRadius = '100%';
+// circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+

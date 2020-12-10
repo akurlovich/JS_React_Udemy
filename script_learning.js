8fdbe4c5d1299fa7a065194f4bf56369341e9c66
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2020-12-10 16:55:13 +0300

    add constructor

--- script_learning.js
+++ script_learning.js
@@ -537,9 +537,32 @@
 
 // console.log(document.documentElement.clientWidth);
 
-//*----------------------------------
+//*----------------------------------конструктор-------------------------------------
 
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
 
+}
+
+User.prototype.exit = function() { //метод exit добаваться у вех
+    console.log(`Пользователь ${this.name} вышел`);
+}
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
 
 
 

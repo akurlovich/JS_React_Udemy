af49b8784c99e2adbceccd2ec7a1a6e291187c95
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2020-12-11 12:21:37 +0300

    add timer

--- Food-dist/js/script.js
+++ Food-dist/js/script.js
@@ -42,7 +42,63 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    //*-------------------------------модальное окно-----------
+    //*-------------------------------добавление таймера------------------------------
+
+    const deadline = '2020-12-20'; // указана дата конца акции
+
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 *60 *24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) { // показывать 0 в 07
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval); 
+            }
+        }
+    }
+
+    setClock('.timer', deadline); 
+
+
+
+    //*-------------------------------модальное окно---------------------------------
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
